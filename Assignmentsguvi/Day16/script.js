d216a4d1fab0edcee4b1a6faa4344847984da271
ranjith24397 <ranjithvimal24397@gmail.com>
2023-07-24 13:27:27 +0530

    Day16

---
+++ Assignmentsguvi/Day16/script.js
@@ -0,0 +1,27 @@
+const greet = (message = "", callback = (n) => {}) => {
+  console.log(message);
+  callback(message);
+};
+
+greet(10, () => {
+  greet(9, () => {
+    greet(8, () => {
+      greet(7, () => {
+        greet(6, () => {
+          greet(5, () => {
+            greet(4, () => {
+              greet(3, () => {
+                greet(2, () => {
+                  greet(1, () => {
+                    greet("Happy Independence Day!!!");
+                  });
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+});
+
